e58b1746567526e7f209649bdb730021800ef8ee
shigemk2 <i.am.shige@gmail.com>
2016-04-02 18:57:35 +0900

    Add sample

--- src/example.jsx
+++ src/example.jsx
@@ -1,24 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import domready from 'domready';
+import Popout from 'react-popout';
 
-const Name = React.createClass({
-    render: function() {
-        return (
-            <span>{this.props.name}</span>
-        );
+class Example extends React.Component {
+    constructor(props) {
+        super(props);
+        this.popout = this.popout.bind(this);
+        this.incrementTimer = this.incrementTimer.bind(this);
+        this.popoutClosed = this.popoutClosed.bind(this);
+        this.popoutContentClicked = this.popoutContentClicked.bind(this);
+        this.state = { isPoppedOut: false, timer: 0 };
     }
-});
-const HelloWorld = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <Name name="Test" />
-            </div>
-        );
+
+    incrementTimer() {
+        var newTimer = this.state.timer + 1;
+        this.setState({ timer: newTimer});
     }
+
+    popout() {
+        this.setState({isPoppedOut: true, timerId: setInterval(this.incrementTimer, 1000)});
+    }
+
+    popoutClosed() {
+        if (this.state.timerId) {
+            clearInterval(this.state.timerId);
+            this.setState({isPoppedOut: false, timerId: null, timer: 0});
+        }
+    }
+
+    popoutContentClicked() {
+        this.popoutClosed();
+    }
+
+    render() {
+        if (this.state.isPoppedOut) {
+            return (
+                // Remove url parameter to see about:blank support
+                <Popout title='Test' onClosing={this.popoutClosed}>
+                    <div>
+                        <div>Popped out content! Timer: {this.state.timer}</div>
+                        <div onClick={this.popoutContentClicked}>Close</div>
+                    </div>
+                </Popout>
+            );
+        } else {
+            return (
+                <div>
+                    <strong>Section <a style={{ textDecoration: 'underline', color: 'blue', cursor: 'pointer' }} onClick={this.popout}>(pop window out)</a></strong>
+                    <div>Inline content</div>
+                </div>
+            );
+        }
+    }
+}
+
+domready(() => {
+    var container = document.createElement('div');
+    document.body.appendChild(container);
+    ReactDOM.render(<Example />, container);
 });
-ReactDOM.render(
-    <HelloWorld />,
-    document.getElementById('content')
-);
